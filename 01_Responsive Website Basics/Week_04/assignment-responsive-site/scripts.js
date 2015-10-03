7d8bf89103e0f8c05b76afca7ff2445e59d8529a
tblong <tyler409@gmail.com>
2015-10-03 14:59:11 -0400

    responsive site progress
    js added to dynamically color rows when clicked on.

--- 01_Responsive Website Basics/Week_04/assignment-responsive-site/scripts.js
+++ 01_Responsive Website Basics/Week_04/assignment-responsive-site/scripts.js
@@ -1,17 +1,42 @@
 // my scripts
 
-function printQuantity() {
-	var val = $("#qty").val();
+// handle form submissions
+function submitForm(form) {
+	event.preventDefault();
+	var item = form.itemBox.value.trim();
+	var qty = $("#qty").val();
 	
-	console.log("quantity is: " + val);
-}
-
-
-function test() {
-	console.log('enter pressed');
-}
-
-// add item to list
-function addItem() {
+	// blank item box
+	form.itemBox.value = "";
 	
+	// valid item check
+	if (item == '') {
+		alert('Please enter an item');
+		document.getElementById('item').focus();
+		return;
+	}
+
+
+
+	console.log('item: ' + item + '\n' + 'quantity: ' + qty);
+
+	// regain focus for item field
+	document.getElementById('item').focus();
 }
+
+// row click handling
+function rowHandler(row) {
+	// check if this item is in the cart yet
+	var incart = row.getAttribute('incart');
+	// console.log("in cart: " + incart);
+	if (incart == 'Yes') {
+		row.setAttribute('incart', 'No');
+		row.lastElementChild.innerHTML = 'No';
+	}
+	else {
+		row.setAttribute('incart', 'Yes');
+		row.lastElementChild.innerHTML = 'Yes'
+	}
+
+}
+
