1b069e9139b3cbbf4ec1005c4191b574fe80e4e7
tblong <tyler409@gmail.com>
2015-11-21 06:28:13 -0500

    data driven progress

--- 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
+++ 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
@@ -1,1 +1,26 @@
 // put your javascript code here
+
+// template variables
+var navCategoryTemplate, categoryTemplate;
+
+
+// helper function to display a template
+function showTemplate(id, template, data) {
+	var html = template(data);
+	$(id).html(html);
+}
+
+
+// doc ready callback
+$(document).ready(function() {
+	
+	// compile templates
+	var source = $('#nav-cat-template').html();
+	navCategoryTemplate = Handlebars.compile(source);
+	showTemplate('#navbar-main', navCategoryTemplate, animals_data);
+	
+	source = $('#category-template').html();
+	categoryTemplate = Handlebars.compile(source);
+	showTemplate('#content', categoryTemplate, animals_data);
+	
+});
