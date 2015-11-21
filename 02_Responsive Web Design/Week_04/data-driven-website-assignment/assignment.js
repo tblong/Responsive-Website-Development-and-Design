bb4205e25d605de150f7811ef9bbe6b7a6300782
tblong <tyler409@gmail.com>
2015-11-21 08:01:16 -0500

    data site progress

--- 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
+++ 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
@@ -3,6 +3,12 @@
 // template variables
 var navCategoryTemplate, categoryTemplate;
 
+// storing the currently displayed category and animal
+var current_category = animals_data.category[0];
+var current_animal = current_category.animals[0];
+
+// track active nav tab
+var active_tab = $('li.menu.active');
 
 // helper function to display a template
 function showTemplate(id, template, data) {
@@ -12,15 +18,20 @@ function showTemplate(id, template, data) {
 
 
 // doc ready callback
-$(document).ready(function() {
+$(document).ready(function () {
 	
 	// compile templates
 	var source = $('#nav-cat-template').html();
 	navCategoryTemplate = Handlebars.compile(source);
 	showTemplate('#navbar-main', navCategoryTemplate, animals_data);
-	
+
 	source = $('#category-template').html();
 	categoryTemplate = Handlebars.compile(source);
 	showTemplate('#content', categoryTemplate, animals_data);
 	
+	console.log('active tab: ' + active_tab.hasClass('active'));
+	active_tab.removeClass('active');
+	console.log('is tab now inactive: ' + active_tab.hasClass('active'));
+	
+
 });
