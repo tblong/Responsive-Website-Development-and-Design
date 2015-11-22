18a86e9b0dcb5c52471992d7baf0f26df34ab2d9
tblong <tyler409@gmail.com>
2015-11-21 23:38:28 -0500

    data site: added templates + some click handling

--- 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
+++ 02_Responsive Web Design/Week_04/data-driven-website-assignment/assignment.js
@@ -1,19 +1,28 @@
 // put your javascript code here
 
 // template variables
-var navCategoryTemplate, categoryTemplate;
+var navCategoryTemplate, categoryTemplate, animalsTemplate, animalTemplate;
 
 // storing the currently displayed category and animal
 var current_category = animals_data.category[0];
 var current_animal = current_category.animals[0];
 
 // track active nav tab
-var active_tab = $('li.menu.active');
+var active_nav_tab = $('li.menu.active');
 
 // helper function to display a template
 function showTemplate(id, template, data) {
 	var html = template(data);
 	$(id).html(html);
+}
+
+// Remove 'active' class from current active tab
+// Add 'active' class to tab id provided
+function updateActiveNavTab(newActiveTabId) {
+	// $('.nav-tabs .active').removeClass('active');
+	active_nav_tab.removeClass('active');
+	active_nav_tab = $(newActiveTabId);
+	active_nav_tab.addClass('active');
 }
 
 
@@ -28,10 +37,51 @@ $(document).ready(function () {
 	source = $('#category-template').html();
 	categoryTemplate = Handlebars.compile(source);
 	showTemplate('#content', categoryTemplate, animals_data);
+
+	source = $('#animals-template').html();
+	animalsTemplate = Handlebars.compile(source);
+
+	source = $('#animal-template').html();
+	animalTemplate = Handlebars.compile(source);
 	
-	console.log('active tab: ' + active_tab.hasClass('active'));
-	active_tab.removeClass('active');
-	console.log('is tab now inactive: ' + active_tab.hasClass('active'));
 	
+	// TODO: add click event handlers
+	
+	// category click handler
+	$('a.nav-link, a.category-thumb').click(function () {
+		// click handler for animal categories
+		
+		// get category index clicked on
+		var index = $(this).data('id');
+		
+		// set current category
+		current_category = animals_data.category[index];
+
+		showTemplate('#content', animalsTemplate, current_category);
+		
+		updateActiveNavTab('#current-category-tab');
+		
+		// animal click handler
+		$('a.animal-thumb').click(function () {
+			// get animal index clicked on
+			var index = $(this).data('id');
+		
+			// set current animal
+			current_animal = current_category.animals[index];
+
+			showTemplate('#content', animalTemplate, current_animal);
+			
+			updateActiveNavTab('#current-animal-tab');
+
+		});
+	});
+	
+
+	
+	
+	// console.log('is it active?: ' + active_tab.hasClass('active'));
+	// active_tab.removeClass('active');
+	// console.log('is it still active?: ' + active_tab.hasClass('active'));
+	// updateActiveNavTab('#current-animal-tab');
 
 });
