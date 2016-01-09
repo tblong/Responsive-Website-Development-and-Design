64b19dfb501506b93c9c6ee9681f52de8f7b7dbb
tblong <tyler409@gmail.com>
2016-01-09 15:36:34 -0500

    week 1: added iframe to textcircle for showing user's html code

--- 04_Web App Dev with JS and MongoDB/Week_01/textcircle/textcircle.js
+++ 04_Web App Dev with JS and MongoDB/Week_01/textcircle/textcircle.js
@@ -1,16 +1,45 @@
+/* global $ */
+/* global Session */
+/* global Mongo */
+/* global Documents */
+/* global Template */
+/* global Meteor */
 this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient) {
+
+    // Session.set("current_date", new Date());
+    
+    // update current_date session key
+    // Meteor.setInterval(function () {
+    //     Session.set("current_date", new Date());
+    // }, 1000);
+    
+    // date_display helpers
+    // Template.date_display.helpers({
+    //     current_date: function () {
+    //         return Session.get("current_date");
+    //     }
+    // });
     
     // editor helpers
     Template.editor.helpers({
+        config: function () {
+            return function (editor) {
+                editor.on("change", function (cm_editor, info) {
+                    // console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                });
+            }
+        },
         docid: function () {
 
             var doc = Documents.findOne();
+            // validate doc
             if (doc) {
                 return doc._id;
             }
-
+            // doc not ready yet
             return undefined;
         }
     });
