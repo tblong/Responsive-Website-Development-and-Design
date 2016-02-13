edfdacffb95a31c4528a2641f32ea8ce6ae90985
tblong <tyler409@gmail.com>
2016-02-12 22:11:30 -0500

    navbar updates

--- 05_ResponsiveWebsiteTutorial/Week_02/enhanced_music_machine/client/router.js
+++ 05_ResponsiveWebsiteTutorial/Week_02/enhanced_music_machine/client/router.js
@@ -1,3 +1,4 @@
+/* global Meteor */
 /* global Router */
 // set up the main template the the router will use to build pages
 Router.configure({
@@ -13,5 +14,9 @@ Router.route('/', {
     action: function () {
         this.render("navbar", { to: "header" });
         this.render("home", { to: "main" });
+    },
+    onAfterAction: function() {
+        console.log($("#mixer"));
+        $("#mixer").toggleClass("active");
     }
 });
