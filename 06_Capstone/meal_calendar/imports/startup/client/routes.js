9a1c0b87f069c4b3fd12173268337e8bcaadbb20
Tyler Long <tblong@localhost.localdomain>
2016-07-24 21:33:01 -0400

    base routes and navbar working

---
+++ 06_Capstone/meal_calendar/imports/startup/client/routes.js
@@ -0,0 +1,23 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Import to load these templates
+import '../../ui/layouts/app-body.js';
+import '../../ui/pages/home.js';
+import '../../ui/pages/navbar.js';
+import '../../ui/pages/app-not-found.js';
+
+// root route
+FlowRouter.route('/', {
+  name: 'app.home',
+  action() {
+    BlazeLayout.render('app_body', { top: 'navbar', main: 'home' });
+  },
+});
+
+// the App_notFound template is used for unknown routes and missing lists
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('app_body', { top: 'navbar', main: 'app_notFound' });
+  },
+};
