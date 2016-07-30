1ddc9c03334e0c683ee322d2c38d007b36ba78be
Tyler Long <tblong@localhost.localdomain>
2016-07-30 19:10:17 -0400

    adding base meals template

--- 06_Capstone/meal_calendar/imports/startup/client/routes.js
+++ 06_Capstone/meal_calendar/imports/startup/client/routes.js
@@ -6,6 +6,7 @@ import '../../ui/layouts/app-body.js';
 import '../../ui/pages/home.js';
 import '../../ui/pages/navbar.js';
 import '../../ui/pages/app-not-found.js';
+import '../../ui/pages/meals/meals.js';
 
 // root route
 FlowRouter.route('/', {
@@ -21,3 +22,11 @@ FlowRouter.notFound = {
     BlazeLayout.render('app_body', { top: 'navbar', main: 'app_notFound' });
   },
 };
+
+// the route to show all the meals create by a user
+FlowRouter.route('/meals', {
+  name: 'app.meals',
+  action() {
+    BlazeLayout.render('app_body', { top: 'navbar', main: 'meals' });
+  },
+});
---
+++ 06_Capstone/meal_calendar/imports/ui/pages/meals/meals.js
@@ -0,0 +1,2 @@
+import './meals.html';
+
