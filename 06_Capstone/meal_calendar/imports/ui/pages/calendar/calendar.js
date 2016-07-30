0c65e8417f85e2648d35904b0a7db55b6f8b7acc
Tyler Long <tblong@localhost.localdomain>
2016-07-30 12:07:10 -0400

    calendar template and supporting js

---
+++ 06_Capstone/meal_calendar/imports/ui/pages/calendar/calendar.js
@@ -0,0 +1,17 @@
+import './calendar.html';
+import { Template } from 'meteor/templating';
+
+Template.fullcalendar.onRendered(function renderCalendar() {
+    const instance = this; // template instance
+    const div = instance.$(instance.firstNode);
+    if (instance.data != null) {
+        // jquery takes care of undefined values, no need to check here
+        div.attr('id', instance.data.id);
+        div.addClass(instance.data.class);
+    }
+    div.fullCalendar(instance.data);
+    console.log('div from fullcalendar');
+    console.log(div);
+    console.log('template instance from fullcalendar');
+    console.log(instance);
+});
