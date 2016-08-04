47946be6afdfb2eb2fb14914c23e4092b1a14300
Tyler Long <tblong@localhost.localdomain>
2016-08-03 21:59:35 -0400

    meal template changes

--- 06_Capstone/meal_calendar/lib/collections.js
+++ 06_Capstone/meal_calendar/lib/collections.js
@@ -1,7 +1,26 @@
 /* global Meals:true */
 /* global Calendars:true */
+/* global SimpleSchema:true */
 
 import { Mongo } from 'meteor/mongo';
 
 Meals = new Mongo.Collection('meals');
 Calendars = new Mongo.Collection('calendars');
+
+
+// schemas
+
+Meals.attachSchema(new SimpleSchema({
+    name: {
+        type: String,
+        label: 'Meal Name',
+    },
+    description: {
+        type: String,
+        label: 'Meal Description',
+        max: 1000,
+        autoform: {
+            rows: 4,
+        },
+    },
+}));
