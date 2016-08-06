b943523622ebb10d8c6661da26011af9e7162c23
Tyler Long <tblong@localhost.localdomain>
2016-08-06 17:41:31 -0400

    meals templates reactive

--- 06_Capstone/meal_calendar/imports/ui/pages/meals/meals.js
+++ 06_Capstone/meal_calendar/imports/ui/pages/meals/meals.js
@@ -1,7 +1,10 @@
 /* global AutoForm:true */
 /* global Meteor:true */
+/* global Meals:true */
+
 import './meals.html';
 import '../footer.js';
+import { Template } from 'meteor/templating';
 
 
 // autoform hooks
@@ -20,5 +23,18 @@ AutoForm.hooks({
                 return false;
             },
         },
+    },
+});
+
+// meal_list helpers
+Template.meal_list.helpers({
+    meals() {
+        const userId = Meteor.userId();
+        return Meals.find({ owner: userId });
+    },
+    hasMeals() {
+        const userId = Meteor.userId();
+        const meals = Meals.find({ owner: userId }).count();
+        return meals > 0;
     },
 });
