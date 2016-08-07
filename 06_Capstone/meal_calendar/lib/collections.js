d1f41de70155ed382754de9e758126399acaa278
Tyler Long <tblong@localhost.localdomain>
2016-08-07 11:13:41 -0400

    adding calendar schema

--- 06_Capstone/meal_calendar/lib/collections.js
+++ 06_Capstone/meal_calendar/lib/collections.js
@@ -8,8 +8,31 @@ Meals = new Mongo.Collection('meals');
 Calendars = new Mongo.Collection('calendars');
 
 
-// schemas
+// calendar schema
+Calendars.attachSchema(new SimpleSchema({
+    name: {
+        type: String,
+        max: 20,
+        label: 'Calendar Name',
+    },
+    description: {
+        type: String,
+        label: 'Calendar Description',
+        max: 1000,
+        autoform: {
+            rows: 4,
+        },
+    },
+    owner: {
+        type: String,
+        optional: true,
+        autoform: {
+            omit: true,
+        },
+    },
+}));
 
+// meals schema
 Meals.attachSchema(new SimpleSchema({
     name: {
         type: String,
